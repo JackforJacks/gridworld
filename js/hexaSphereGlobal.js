1924119766393aefc9d1c9fd163ae10957667333
JackforJacks <giacomomosso@gmail.com>
2025-05-27 20:59:19 +0200

    cleaned up hexasphere.js

--- js/hexaSphereGlobal.js
+++ js/hexaSphereGlobal.js
@@ -1,17 +1,22 @@
-// Expose THREE and Hexasphere globally
+/**
+ * hexaSphereGlobal.js
+ * This file exposes THREE and Hexasphere globally and handles module exports
+ * The bundling is handled by Grunt/Browserify
+ */
+
+// Import dependencies
 window.THREE = require('three');
 const Hexasphere = require('../src/hexaSphere');
+
+// Expose Hexasphere globally for browser access
 window.Hexasphere = Hexasphere;
 
-// If using ECSY
-window.ECSY = require('ecsy');
-
-// For CommonJS environments
+// Export for CommonJS environments
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = Hexasphere;
 }
 
-// For ES modules
+// Export for ES modules
 if (typeof exports !== 'undefined') {
     exports.default = Hexasphere;
 }
